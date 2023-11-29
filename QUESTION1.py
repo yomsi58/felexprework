b2097292b8970908b684dac027cad25260bead7f
Raymond <feuzeu58@gmail.com>
2023-11-29 15:15:08 -0500

    first commit

---
+++ QUESTION1.py
@@ -0,0 +1,10 @@
+#write a function to print " hello-username ", 
+ # USERNAME  IS the input of the function . the first line of the code  has  been defined as below
+#def
+#hello_name(user_name)  
+# RAYMOND FEUZEU USERNAME RAYMOND58
+def hello_name(user_name):
+    user_name= input("my username is ")
+    print(" hello_", user_name)
+
+hello_name("user_name")    
